94b87b453dec03af921090bcd4e64c1365adf9da
Stig Thomas Gulbrandsen <13stigul@gmail.com>
2019-11-25 11:43:38 +0100

    Initial commit

---
+++ blob_Island_Adventure/Adventure_Functionality.h
@@ -0,0 +1,17 @@
+#ifndef ADV_FUNC_H
+
+//Process text input
+//Go to another screen/scene
+//Display help
+//Display inventory
+//Display status
+//look at stuff
+//kick stuff
+//stab/slice/cut/attack stuff
+//talk to stuff
+//obtain item
+//Use item
+#endif
+
+
+
